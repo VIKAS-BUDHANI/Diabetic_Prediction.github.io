3d2af64e712ac12ecceb2c622ef676e3fd62998f
Vikas Budhani <111237089+VIKAS-BUDHANI@users.noreply.github.com>
2023-02-10 23:55:54 +0530

    -

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def home():
         prediction = model.predict(scaler.transform(input_features))
         #print(prediction)
 
-    return render_template('main.html', prediction=prediction)
+    return render_template('index.html', prediction=prediction)
 
 if __name__ == '__main__':
     app.run(debug=True)
